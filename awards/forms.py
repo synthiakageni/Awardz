60eac53be55360157accfa765e18674f1201cfa7
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-13 12:05:01 +0300

    import generic from django.views

--- awards/forms.py
+++ awards/forms.py
@@ -1,2 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.models import User
+from django.views import generic
+from django.views.generic import UpdateView
+from .models import Profile,Project, Rate
