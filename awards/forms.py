4c25ff482f25c5358d5cf4a3b52f8a4118dbc417
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-13 12:09:29 +0300

    create profile form

--- awards/forms.py
+++ awards/forms.py
@@ -11,5 +11,12 @@ class UserSignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        
+ class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields = [
+            'bio',
+            'profile_pic',
+            'twitter_url'
+        ]       
 
