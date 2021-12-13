236f31dac29e50aa35132a120d72862861a0eff1
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-13 12:10:24 +0300

    create profile update form

--- awards/forms.py
+++ awards/forms.py
@@ -11,12 +11,17 @@ class UserSignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
- class ProfileForm(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         fields = [
             'bio',
             'profile_pic',
             'twitter_url'
-        ]       
+        ] 
+class ProfileUpdateForm(forms.ModelForm):
+            class Meta:
+                model=Profile
+                fields = ['bio','profile_pic','twitter_url']        
+
 
