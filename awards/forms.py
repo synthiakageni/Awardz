d2d5c98c1f5c6e96d53cfba6462c38680561d1ca
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-13 12:08:55 +0300

    create signup form

--- awards/forms.py
+++ awards/forms.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import User
 from django.views import generic
 from django.views.generic import UpdateView
 from .models import Profile,Project, Rate
+
+class UserSignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=100,help_text='Required')
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        
+
