3dc93447d969c6065ac83518b6681af31419be98
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-13 11:57:41 +0300

    create function editProfile

--- awards/views.py
+++ awards/views.py
@@ -86,4 +86,23 @@ def login(request):
         login(request,user)
 def logout(request):
     if request.method == "POST":
-        logout(request)        
+        logout(request)   
+def editProfile(request):
+    if request.method=='POST':
+        user_form=UserUpdateForm(request.POST, instance=request.user)
+        profile_form=ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form=UserUpdateForm(instance=request.user)   
+        profile_form=ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "user_form":user_form,
+        "profile_form":profile_form,
+        
+    }
+    return render (request, "accounts/edit_profile.html",context)  
+                
