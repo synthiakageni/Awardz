957c3fd741d627a3f270b0ac5fee8c108564f109
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-16 11:57:18 +0300

    update

--- awards/views.py
+++ awards/views.py
@@ -32,84 +32,84 @@ class edit_profile(generic.UpdateView):
     fields=['bio','profile_pic','twitter_url']
     success_url=reverse_lazy('')
 
-# def usersignup(request):
+def usersignup(request):
      
-#     message = 'CREATE ACCOUNT !'
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password1']
-#             user = authenticate(username=username, password=password)
-#             login(request)
-#             messages.success(request,("Your account has  succesfully been created!"))
-#             return redirect('welcome')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'accounts/signup.html',{"message":message, "form":form})
+    message = 'CREATE ACCOUNT !'
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request)
+            messages.success(request,("Your account has  succesfully been created!"))
+            return redirect('welcome')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html',{"message":message, "form":form})
     
-# def activate_account(request, uidb64, token):
-#     try:
-#         uid = force_bytes(urlsafe_base64_decode(uidb64))
-#         user = User.objects.get(pk=uid)
-#     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-#         user = None
-#     if user is not None and account_activation_token.check_token(user, token):
-#         user.is_active = True
-#         user.save()
-#         login(request, user)
-#         return HttpResponse('Your account has been activate successfully')
+def activate_account(request, uidb64, token):
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Your account has been activate successfully')
         
-#     else:
-#         return HttpResponse('Activation link is invalid!')
-# def login(request):
+    else:
+        return HttpResponse('Activation link is invalid!')
+def login(request):
    
-#     message = 'Sign In!'
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = authenticate(username=username, password=password)
+    message = 'Sign In!'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password1']
+        user = authenticate(username=username, password=password)
 
-#         if user is not None:
-#             login(request)
-#             messages.success(request,f" Hello, {username} welcome to clicksgram")
-#             return redirect('welcome')
-#         else:
-#             messages.success(request,"sorry,try login in again")
-#             return render(request,'accounts/login.html')
-#     else:
-#         return render(request, 'accounts/login.html',{"message":message})
-# def logout(request):
-#     if request.method == "POST":
-#         logout(request) 
-# def view_profile(request):
+        if user is not None:
+            login(request)
+            messages.success(request,f" Hello, {username} welcome to clicksgram")
+            return redirect('welcome')
+        else:
+            messages.success(request,"sorry,try login in again")
+            return render(request,'accounts/login.html')
+    else:
+        return render(request, 'accounts/login.html',{"message":message})
+def logout(request):
+    if request.method == "POST":
+        logout(request) 
+def view_profile(request):
  
-#     return render (request, "accounts/profile.html")  
+    return render (request, "accounts/profile.html")  
               
-# def editProfile(request):
-#     if request.method=='POST':
-#         user_form=UserUpdateForm(request.POST, instance=request.user)
-#         profile_form=ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user_form.save()
-#             profile_form.save()
-#             return redirect('profile')
-#     else:
-#         user_form=UserUpdateForm(instance=request.user)   
-#         profile_form=ProfileUpdateForm(instance=request.user.profile)
+def editProfile(request):
+    if request.method=='POST':
+        user_form=UserUpdateForm(request.POST, instance=request.user)
+        profile_form=ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form=UserUpdateForm(instance=request.user)   
+        profile_form=ProfileUpdateForm(instance=request.user.profile)
 
-#     context = {
-#         "user_form":user_form,
-#         "profile_form":profile_form,
+    context = {
+        "user_form":user_form,
+        "profile_form":profile_form,
         
-#     }
-#     return render (request, "accounts/edit_profile.html",context)  
-# class PasswordsChangeView(PasswordChangeView):
-#     form_class = PasswordChangeForm
-#     success_url = reverse_lazy('welcome')  
+    }
+    return render (request, "accounts/edit_profile.html",context)  
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('welcome')  
 
-# @login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/login/')
 def new_project(request):
     current_user = request.user
     if request.method == 'POST':
@@ -129,7 +129,7 @@ def view_projects(request):
     return render(request, 'index.html',{"projects":projects,"form":form})
 
 
-# @login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/login/')
 
 def rate_project(request,id):
     # reviews = Revieww.objects.get(projects_id = id).all()
